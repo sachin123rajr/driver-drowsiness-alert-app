a717f871c10b2cc57ddf08dc3f74fc9a82e556fb
Sachin mandal <sachinmandaldevid@gmail.com>
2020-02-15 21:39:27 +0530

    First commit

---
+++ app/src/main/java/com/example/callback/drowsiness/MyPreferences.java
@@ -0,0 +1,20 @@
+package com.example.callback.drowsiness;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MyPreferences {
+    private static final String MY_PREFERENCES = "my_preferences";
+
+    public static boolean isFirst(Context context){
+        final SharedPreferences reader = context.getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
+        final boolean first = reader.getBoolean("is_first", true);
+        if(first){
+            final SharedPreferences.Editor editor = reader.edit();
+            editor.putBoolean("is_first", false);
+            editor.commit();
+        }
+        return first;
+    }
+
+}
